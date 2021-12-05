2cb8d38202117655e0458ed3e4b8fb2d0f6fd460
korneyqq <novikov-korney@inbox.ru>
2021-12-05 17:57:21 +0300

    add map

--- map.py
+++ map.py
@@ -15,4 +15,4 @@ world_map = set()
 for j,row in enumerate(text_map):
     for i, char in enumerate(row):
         if char == 'W':
-            world_map.add((i * TILE))
+            world_map.add((i * TILE, j * TILE))
